36672ee6be84a8883922e3adc975a1c94b33c9b9
dharma111 <dharmaraj96ramachandran@gmail.com>
2016-09-25 20:03:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,15 @@ console.log('Loaded!');
 var element = document.getElementById("text-content");
 var madi = document.getElementById('img');
 madi.onclick=function(){
-    madi.style.marginLeft='100px';
+    var time=setInterval(moveRight,100);
 };
+var leftmargin=0;
+function moveRight()
+{
+    leftmargin=leftmargin+10;
+img.style.marginLeft=leftmargin+'px';
+    
+}
+
 
 
