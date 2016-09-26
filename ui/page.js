d860ec5c0739e6a7ef8e4de23d2ece1d80d372bc
dharma111 <dharmaraj96ramachandran@gmail.com>
2016-09-26 06:27:59 +0530

    [imad-console] Updates ui/page.js

--- ui/page.js
+++ ui/page.js
@@ -1,2 +1,10 @@
 //Javascript for second page :)
-
+//counting number of clicks
+var clicks=0;
+var counting=document.getElementById('counter');
+counting.onclick=function()
+{
+    var number=document.getElementById('count');
+     clicks=clicks+1;
+    number.innerHTML = clicks.toString();
+};
