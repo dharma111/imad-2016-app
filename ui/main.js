dcdc51d078f3f1efbf6a37a49f5fa28549504e9a
dharma111 <dharmaraj96ramachandran@gmail.com>
2016-09-26 06:46:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,8 +20,9 @@ var clicks=0;
 var counting=document.getElementById('counter');
 counting.onclick=function()
 {
-    var number=document.getElementById('count');
+    
      clicks=clicks+1;
+     var number=document.getElementById('count');
     number.innerHTML = clicks.toString();
 };
 
