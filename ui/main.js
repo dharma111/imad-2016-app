fcd12b2e1ed9fbb82785f6ea7cec772feca3410e
dharma111 <dharmaraj96ramachandran@gmail.com>
2016-09-26 06:48:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,7 @@
 console.log('Loaded!');
 //Moving madi on a click in page1
 
-var image=document.getElementById('img');
-var rightmargin=0;
-function moveLeft()
-{
-    rightmargin=rightmargin+10;
-    image.style.marginLeft=rightmargin+'px';
 
-}
-
-image.onclick= function(){
-
-    var int = setInterval(moveLeft,100);
-};
 //Javascript for second page :)
 //counting number of clicks
 var clicks=0;
